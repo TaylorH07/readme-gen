d22c2eeda130a8634bf923466e1cda4cfde6d383
taylor.holmes <taylorh2343@gmail.com>
2022-01-24 00:19:03 -0600

    updated readme file function to function properly

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const path = require('path');
+const util = require('util');
 const generateMarkdown = require('./utils/generateMarkdown')
 
 // TODO: Create an array of questions for user input
@@ -16,21 +16,29 @@ const questions = [
         name: 'license',
         message: 'What license would like to use?',
         choices:  ['MIT', 'APACHE 2.0']
-    }
+    }, 
 ];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-    fs.writeFileSync(path.join(process.cwd(), "output", fileName), data)
+    fs.writeFile(fileName, data, err => {
+        if (err) {
+          return console.log(err);
+        }
+      
+        console.log("Congrats! You have generated your README.md file!")
+    });
 }
+
+const writeFileAsync = util.promisify(writeToFile);
+
 
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then(answers => {
         const markdown = generateMarkdown(answers);
         writeToFile('README.md', markdown)
-        })
-    }
-
+    })
+};
 // Function call to initialize app
 init();
